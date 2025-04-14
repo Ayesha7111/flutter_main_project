2185b57bf8b1543e6c77e101493d38ee2327f856
Ayesha7111 <ayeshamustafa267@gmail.com>
2025-04-14 19:42:45 +0500

    Add files via upload

---
+++ view_data_page.dart
@@ -0,0 +1,12 @@
+// view_data_page.dart
+import 'package:flutter/material.dart';
+
+class ViewDataPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("View Data")),
+      body: Center(child: Text("This is the View Data Page")),
+    );
+  }
+}
